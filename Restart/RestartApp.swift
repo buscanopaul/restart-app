e5907b6ae5753638b8fc46ed7515ef72bdb8134f
Paul Buscano <pbuscano@deloitte.com>
2022-04-04 16:47:08 +0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by pbuscano on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
